5834c9fb15ffa1a07d08244eb7bb7b94223cea12
Victor Cordero <victordcordero@gmail.com>
2020-11-11 23:07:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   final_results = data.each_with_object({}) do |(key, value), final_array|
-    value.each do |inner_key, names| 
+    value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name]
         !final_array[name] = {}
