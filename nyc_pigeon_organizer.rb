278003b1250a0d108bc6b9c2c439af971b7aadd6
Victor Cordero <victordcordero@gmail.com>
2020-11-11 23:08:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,4 +8,9 @@ def nyc_pigeon_organizer(data)
       end
       if !final_array[name][key]
       !final_array[name][key] = []
+    end
+    final_array[name][key].push(inner_key.to_s)
+  end
+end
+end
 end
